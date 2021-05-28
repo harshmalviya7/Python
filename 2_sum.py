421da436da5071dbd0818d657e6a3652d6b87f93
Harshmalviya7 <harsh.malviya.9869@gmail.com>
2021-05-28 13:36:51 +0530

    Code

---
+++ 2_sum.py
@@ -0,0 +1,18 @@
+
+#return index of value which sums to a given no.
+# [ 2, 7, 11, 15 ] , target=9
+# return [0,1] as 2+7 gives 9
+
+def two_sum(target,nums):
+    hash_table={}
+
+    for i in range(len(nums)):
+        complement=target-nums[i]
+        if complement not in hash_table:
+            hash_table[nums[i]]=i
+        else:
+            return [hash_table[complement],i]
+
+nums=[2,7,11,15]
+target=9
+print(two_sum(target,nums)) #[0, 1]
